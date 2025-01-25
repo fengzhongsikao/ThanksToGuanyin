139e3bb9f5a9f208368d6ce590992817544a89fc
fong <dart@dart.com>
2025-01-25 17:05:59 +0800

    feat:引入mui

---
+++ src/views/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import image1 from '../assets/gruanyin.jpg'
+export default function Home() {
+  return (
+    <div>
+        <img src={image1} alt='观音'></img>
+    </div>
+  )
+}
+
